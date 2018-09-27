450a6f4e75cf4428d1880bd174cfba45b0a46faf
VamsiKrishna143 <43640715+VamsiKrishna143@users.noreply.github.com>
2018-09-27 17:06:58 +0530

    Initial commit

---
+++ src/qsp/DemoA.java
@@ -0,0 +1,13 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA {
+@Test
+public static void testA()
+{
+	Reporter.log("Test A",true);
+}
+}
+
